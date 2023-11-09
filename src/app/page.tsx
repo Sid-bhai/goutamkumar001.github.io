e7fadd34eed2ba6bc433120bce02cb0f6a2c0379
goutamkumar001 <150309376+goutamkumar001@users.noreply.github.com>
2023-11-09 08:19:18 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,6 @@ import Hero from "@/components/main/Hero";
 import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
 import Image from "next/image";
-<<<<<<< HEAD
-import { Hero, Navbar, Social, Footer, Globe, Projects, Tawk } from "../components"
-
-export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <Tawk />
-      <br />   <br />   <br />   <br />
-      <Hero />
-      <Projects />
-      <Globe />
-      <Social />
-      <Footer />
-    </div>
-=======
 
 export default function Home() {
   return (
@@ -30,6 +14,5 @@ export default function Home() {
         <Projects />
       </div>
     </main>
->>>>>>> 1a457d1 (Hello)
   );
 }
