fa474c07f46d030958cccc1f9da2c02ea90170a4
goutamkumar001 <150309376+goutamkumar001@users.noreply.github.com>
2023-11-09 18:18:50 +0530

    Update HeroContent.tsx

--- src/components/sub/HeroContent.tsx
+++ src/components/sub/HeroContent.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import { Typewriter } from "react-simple-typewriter";
 import { motion } from "framer-motion";
 import {
   slideInFromLeft,
@@ -34,26 +33,12 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-          i a'm
+            Providing
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              Goutam Kumar{" "}
+              the best{" "}
             </span>
-            <br />
-            <span className="text-1xl font-bold text-white">
-            And i am a{" "}
-            </span>
-          <span className="text-2xl font-bold text-red-500">
-          <Typewriter
-            words={["Developer", "Coder", "YouTuber", "Student"]}
-            loop={0}
-            cursor
-            cursorStyle="_"
-            typeSpeed={80}
-            deleteSpeed={80}
-            delaySpeed={1000}
-          />
-        </span>
+            project exprience
           </span>
         </motion.div>
 
